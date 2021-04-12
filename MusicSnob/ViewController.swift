86a01d202bee01ed2840842bd65551cb83878d72
Cate Miller <cate@catemarie.dev>
2021-04-11 22:21:48 -0700

    connect genre picker to actual data

--- MusicSnob/ViewController.swift
+++ MusicSnob/ViewController.swift
@@ -7,20 +7,44 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
+    
     @IBOutlet weak var genrePicker: UIPickerView!
     @IBOutlet weak var zipcodeField: UITextField!
     @IBOutlet weak var goButton: UIButton!
     
-    var test: String?
+    var genreData: [String] = [String]()
+    var genreSelection = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.genrePicker.delegate = self
+        self.genrePicker.dataSource = self
+
+        genreData = ["House", "Trance"]
+    }
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return genreData.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return genreData[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        print("Current genre selection changed to: " + genreData[row])
+        genreSelection = row
     }
 
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if let vc = segue.destination as? EventTableViewController{
-            vc.searchParams.genre = "Trance"
+            vc.searchParams.genre = genreData[genreSelection]
             vc.searchParams.zipcode = "92104"
         }
     }
